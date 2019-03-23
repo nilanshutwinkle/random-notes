f4841df5f3d2a0954702a8b7d1d578690d7dff2e
Bryan Smith <bryanesmith@gmail.com>
2019-03-23 16:03:24 -0400

    progress on script to cleanup grammar format

---
+++ Korean Grammar/migrate-to-v2.py
@@ -0,0 +1,61 @@
+#
+# Usage: python migrate-to-v2.py <path-to-input> > <path-to-output>
+#
+#   Migrate README.md v1 to v2. Use Python 3.
+#
+import os.path
+import re
+import sys
+
+# Logs an msg to stderr. (stdout reserved for outputting file.)
+def log(msg: str):
+    print(msg, file=sys.stderr)
+
+# Exit with error msg and non-zero status code.
+def error(msg: str, exit_code: int):
+    log(msg)
+    sys.exit(exit_code)
+
+# Check arguments
+if len(sys.argv) != 2:
+    error("Expecting one argument: <path-to-input>", 1)
+
+input = sys.argv[1]
+
+if not os.path.isfile(input):
+    error("File not found: {}".format(input), 2)
+
+# Read in file, and identify new grammars and supporting examples.
+#   Store map of grammars to lines.
+curr_grammar = None
+grammars = {}
+with open(input) as i:
+    for line in i:
+        line = line.strip()
+        # Discard categories and blank line
+        if line.startswith("#") or line == "":
+            #log("Skipping line: {}".format(line))
+            pass
+        # New grammar
+        elif line.startswith(">"):
+            curr_grammar = line
+            if curr_grammar in grammars:
+                error("Grammar already exists, please dedupe source file: {}".format(curr_grammar), 3)
+            grammars[curr_grammar] = []
+        else:
+            if not curr_grammar:
+                error("No grammar found, not sure what to do with this line: {}".format(line), 4)
+            grammars[curr_grammar].append(line)
+
+# Print out TOC
+print("# Korean grammar")
+print()
+print("## Contents")
+
+grammar_english_part = re.compile('\(.*?\)')
+
+for grammar in grammars:
+    clean_grammar = grammar_english_part.sub('', grammar)
+    print("* [{}](#{})".format(clean_grammar, hash(grammar)))
+
+# Print out contents
